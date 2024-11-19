962d2c54f01b73d7af4683ba7b646e9fad99e570
emilio.dimari <emilio.dimari@cws.it>
2024-11-19 18:04:27 +0100

    footer detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
-// import { ServicesComponent } from './servicesRestoran/services.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContactComponent } from './contact/contact.component';
 import { BookingComponent } from './booking/booking.component';
 import { TeamComponent } from './team/team.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ServicesRestorantComponent } from './servicesRestorant/services-restorant/services-restorant.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'about', component: AboutComponent },
-  // { path: 'services', component: ServicesComponent },
+  { path: 'services', component: ServicesRestorantComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'booking', component: BookingComponent },
